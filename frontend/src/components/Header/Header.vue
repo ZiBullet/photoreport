7e30ac287fb5dbc17614f1b2f23925f964442678
ZiBullet <zamolidinziadulaev@gmail.com>
2024-10-23 20:29:54 +0500

    Header - Banner done

--- frontend/src/components/Header/Header.vue
+++ frontend/src/components/Header/Header.vue
@@ -1,10 +1,11 @@
 <template>
-    <section class="banner bg-black">
-        <div class="container 2xl mx-auto px-3.5">
-            <header class="header py-11">
+    <section class="banner">
+        <header class="header py-11 absolute top-0 left-0 z-10 w-full">
+            <div class="container 2xl mx-auto px-3.5">
                 <nav class="header__nav flex justify-between gap-4 items-center">
-                    <router-link class="header__nav-logo w-40 flex items-center justify-center" to="/">
-                        <img class="w-full h-full object-cover" src="@assets/icons/logo-wh.svg" alt="logo">
+                    <router-link class="header__nav-logo md:w-40 w-6 flex items-center justify-center" to="/">
+                        <img class="md:block hidden w-full h-full object-cover" src="@assets/icons/logo-wh.svg" alt="logo">
+                        <img class="md:hidden block w-full h-full object-cover" src="@assets/icons/logo-phones.svg" alt="logo">
                     </router-link>
                     <ul class="header__nav-list lg:flex items-center lg:gap-10 hidden xl:gap-16 ">
                         <li class="header__nav-list-item flex justify-center items-center">
@@ -24,7 +25,8 @@
                         </li>
                     </ul>
                     <div class="header__nav-links flex items-center gap-9">
-                        <router-link to="/search" class="header__nav-link lg:flex hidden text-2xl items-center justify-center">
+                        <router-link to="/search"
+                            class="header__nav-link lg:flex hidden text-2xl items-center justify-center">
                             <span class="flex items-center justify-center">
                                 <i class="icon-search text-2xl"></i>
                             </span>
@@ -40,7 +42,7 @@
                         </button>
                     </div>
                 </nav>
-            </header>
+            </div>
             <Sidebar :isOpen="isOpen" @closeMenu="isOpen = false" :burger-icon="false" right disableOnOutsideClick>
                 <router-link class="menu-item" to="/">
                     Главная
@@ -64,7 +66,25 @@
                     Профиль
                 </router-link>
             </Sidebar>
+        </header>
 
+        <div class="banner__wrap w-full h-svh max-h-[900px] min-h-[376px] relative">
+            <img src="@assets/imgs/poster.webp" alt="" class="select-none absolute w-full h-full object-cover">
+            <div class="px-4 h-full flex flex-col justify-center container 2xl mx-auto">
+                <div class="banner__desc mb-4 relative z-[6] flex flex-col items-start max-w-[538px] w-full">
+                    <h4 class="banner__desc-title font-medium text-sm uppercase mb-[15px]">
+                        АРТ ПРОСТРАНСТВО KLUMBA SPACE
+                    </h4>
+                    <p class="banner__desc-text mb-7 xl:text-5xl sm::text-4xl  text-2xl font-bold">
+                        Кавер-бэнд <br> «В Главных ролях »
+                    </p>
+                    <a href="#" class="banner__desc-link flex items-center justify-center gap-[15px] font-bold text-sm">
+                        <span>Посмотреть</span>
+                        <img src="@assets/icons/arrow-right.svg" alt="">
+                    </a>
+                </div>
+            </div>
+            <img src="@assets/icons/heart-sm.svg" alt="" class="lg:hidden absolute z-[4] bottom-[6%] right-[40%]">
         </div>
     </section>
 </template>
